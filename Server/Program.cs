18e699503291c203083e98dc7ebd8b28d8db555a
Julian Torres <jntorres2012@gmail.com>
2024-08-03 00:11:21 -0300

    error arreglado, migrations add

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,12 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //sqlserver 
 builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-//builder.Services.AddDbContext<DataContext>(options =>
-//	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefConct")));
 
 
 
